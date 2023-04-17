8a5e8e649c5d95d8a3e415488061010988b3ca64
ghYeet42 <carterfscarlett@gmail.com>
2023-04-17 14:54:43 -0400

    first things on js

---
+++ grade.js
@@ -0,0 +1,31 @@
+console.log("hello world")
+
+let myAvg = 99
+
+if (myAvg > 0 && myAvg < 59.99) {
+    console.log("Your letter grade is an F.")
+} else if (myAvg < 69.99 && myAvg > 60) {
+    console.log ("Your letter grade is a D.")
+} else if (myAvg < 79.99 && myAvg > 70) {
+    console.log ("Your letter grade is a C.")
+} else if (myAvg < 89.99 && myAvg > 80) {
+    console.log ("Your letter grade is a B.")
+} else if (myAvg < 99.9 && myAvg > 90) {
+    console.log ("Your letter grade is a A.")
+} else if (myAvg >= 100) {
+    console.log ("Your letter grade is a A+.")
+} else if (myAvg <= 0) {
+    console.log ("..you're screwed.")
+}
+
+let myList = ["Filet", "Fileto", "Filetomignon", "Jellyfam", "Guapo", "Guap", "Guavahector", "Jellyman" ]
+let myList1 = [5, 20, 400]
+
+
+for (let item of myList) {
+    console.log(item)
+}
+
+for (let item of myist1) {
+    console.log(item) * 6
+}
---
+++ hello.js
@@ -0,0 +1,31 @@
+console.log("hello world")
+
+let myName = ("Carterwarterr")
+let myAge = 20
+let myAge2 = 200
+
+console.log("Hello my name is " + myName + ". I am " + myAge2 + " years old.")
+
+if (myAge == 20) {
+    console.log("Sorry, my age is actually 20..")
+} else if (myAge) {
+    console.log ("Yea.. I'm actually " + myAge2 + " years old.")
+}
+
+
+if (myName == "Carterwarter") {
+    console.log("Yea.. my name is actually Carterwarter.")
+} else {
+    console.log("Sorry, my actual name is Carter..")
+}
+
+myList = (5, 30, 300)
+myList1 = ("Dinero", "Dollabillz", "Pesos", "Guapo")
+
+for (let item of myList) {
+    console.log(item) * 3
+}
+
+for (let item of myList1) {
+    console.log(item) 
+}
