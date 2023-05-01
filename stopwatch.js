87bd539ed24501a006e4d4693cf319ff2fc98293
ghYeet42 <carterfscarlett@gmail.com>
2023-05-01 14:51:34 -0400

    stopAwatchA almost done

--- stopwatch.js
+++ stopwatch.js
@@ -1,11 +1,51 @@
+let ms = 0
+
+let secs = 0
+
+let mins = 0
+
+let digital = document.getElementById("digital")
+
+let interval;
+
 function startTheTime () {
-    alert ("yo")
+
+    resetTheTime()
+    
+    interval = setInterval(function () {
+       
+        ms = ms + 10
+        
+        if (ms >= 1000) {
+            secs = secs + 1
+            ms = 0
+        }
+
+        if (secs >= 60) {
+            mins = mins + 1
+            secs = 0
+        }
+
+        digital.innerHTML = mins + ':' + String(secs).padStart(2, '0') + ':' + ms
+
+    }, 10)
+
 }
+
 
 function stopTheTime () {
-    alert ("wasg")
+    clearInterval(interval)
 }
 
+
 function resetTheTime () {
-    alert ("heyyoo")
+       
+        stopTheTime()
+
+        ms = 0
+        secs = 0
+        mins = 0
+
+        digital.innerHTML = '00:00:00'
+
 }
